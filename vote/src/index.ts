51d92b8d2830d46c9fe8763be1499d12690aad81
Joseph Petrillo <jpetrillo119@gmail.com>
2022-11-29 16:41:21 -0500

    update setup of vote

--- vote/src/index.ts
+++ vote/src/index.ts
@@ -1,44 +1,43 @@
 import amqplib from "amqplib";
 import express from "express";
 
-// Connect to rabbitmq, create a channel
+// Connect to RabbitMQ and create a communication channel
 const eventBusConnection = await amqplib.connect("amqp://event-bus:5672");
 const eventBusChannel = await eventBusConnection.createChannel();
 
-const queue: string = "vote";
-const exchange: string = "event-bus";
+const queue = "vote";
+const exchange = "event-bus";
 
-// Create exchange
+// Assert exchange
 eventBusChannel.assertExchange(exchange, "direct", {
   durable: false,
 });
 
-// Create queue for service
+// Assert queue
 eventBusChannel.assertQueue(queue);
 
-const eventKeys: string[] = ["room-events", "expiration-events"];
+const eventKeys = ["room-events", "expiration-events"];
 
 // Subscribe to each event key
 eventKeys.forEach((key: string) => {
   eventBusChannel.bindQueue(queue, exchange, key);
 });
 
-type Event = any;
-
-const app = express();
-app.use(express.json());
+type Event = any; // TODO types for each event
 
 // Listen for incoming messages
-eventBusChannel?.consume(queue, (message: amqplib.ConsumeMessage | null) => {
+eventBusChannel.consume(queue, (message) => {
   if (message !== null) {
     const { type, data }: Event = JSON.parse(message.content.toString());
 
     switch (type) {
       default:
-      // Insert logic for various events here depending on the type and data
-      // Case for each event type
+      // Insert logic for various events here depending on the type
     }
 
     eventBusChannel.ack(message);
   }
 });
+
+const app = express();
+app.use(express.json());
