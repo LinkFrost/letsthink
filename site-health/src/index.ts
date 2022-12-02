e6ee58a01e6c8ceb6e24b2c6e8735a38ea6c4cdf
sid2033 <siddharthraju299@gmail.com>
2022-12-02 13:57:14 -0500

    Mongo working

--- site-health/src/index.ts
+++ site-health/src/index.ts
@@ -1,6 +1,50 @@
 import amqplib from "amqplib";
 import initRabbit from "./initRabbit.js";
+import express from "express";
+import cors from "cors";
+import { MongoClient, WithId, ObjectId } from "mongodb";
+
 import type { MessageModerated, MessageVoted, PollVoted, RoomCreated, RoomExpired, UserCreated, HTTPRequest } from "./events.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const uri = "mongodb://root:rootpassword@letsthink-site-health-db:27017/?authMechanism=DEFAULT";
+const client = new MongoClient(uri);
+const database = client.db("site-health");
+const collection = database.collection("site-health-data");
+
+// Initializing db
+try {
+  // create a document to insert
+  const findResult = (await collection.find().toArray()) as SiteHealthData[];
+
+  let docs: SiteHealthData[] = [];
+
+  await findResult.forEach((doc) => {
+    docs.push(doc);
+  });
+
+  if (docs.length < 1) {
+    const doc = {
+      totalRooms: 0,
+      activeRooms: 0,
+      expiredRooms: 0,
+      pollRooms: 0,
+      messageRooms: 0,
+      totalVotes: 0,
+      totalMessages: 0,
+      requestRate: 0,
+      totalUsers: 0,
+      errorRate: 0,
+    };
+
+    await collection.insertOne(doc);
+  }
+} catch (e) {
+  console.log(e);
+}
 
 const { eventBusChannel, confirmChannel } = await initRabbit("site-health", [
   "RoomCreated",
@@ -12,7 +56,8 @@ const { eventBusChannel, confirmChannel } = await initRabbit("site-health", [
   "UserCreated",
 ]);
 
-interface SiteHealthData {
+interface SiteHealthData extends WithId<Document> {
+  _id: ObjectId;
   totalRooms: number;
   activeRooms: number;
   expiredRooms: number;
@@ -28,10 +73,10 @@ interface SiteHealthData {
 type Event = RoomCreated | RoomExpired | MessageModerated | UserCreated | PollVoted | MessageVoted | HTTPRequest;
 
 // Listen for incoming messages
-eventBusChannel.consume("site-health", (message) => {
+eventBusChannel.consume("site-health", async (message) => {
   if (message !== null) {
     const { key, data }: Event = JSON.parse(message.content.toString());
-    const siteHealthData: SiteHealthData = fetchSiteHealthData();
+    const siteHealthData: SiteHealthData = await fetchSiteHealthData();
 
     switch (key) {
       case "RoomExpired":
@@ -72,8 +117,13 @@ eventBusChannel.consume("site-health", (message) => {
         break;
       }
       case "HTTPRequest": {
-        siteHealthData.errorRate += 1;
-        updateDatabase(siteHealthData);
+        const updateDoc = {
+          $set: {
+            errorRate: siteHealthData.errorRate + 1,
+          },
+        };
+
+        const result = await collection.updateOne({}, updateDoc);
         break;
       }
     }
@@ -83,23 +133,30 @@ eventBusChannel.consume("site-health", (message) => {
   }
 });
 
-const fetchSiteHealthData = () => {
-  const mockData: SiteHealthData = {
-    totalRooms: 0,
-    activeRooms: 0,
-    expiredRooms: 0,
-    pollRooms: 0,
-    messageRooms: 0,
-    totalVotes: 0,
-    totalMessages: 0,
-    requestRate: 0,
-    totalUsers: 0,
-    errorRate: 0,
-  };
+const fetchSiteHealthData = async () => {
+  const findResult = (await collection.find().toArray()) as SiteHealthData[];
 
-  return mockData;
+  let docs: SiteHealthData[] = [];
+
+  await findResult.forEach((doc) => {
+    docs.push(doc);
+  });
+
+  return docs[0];
 };
+
+app.post("/site-health", async (req, res) => {
+  // send event to rabbitMQ
+  const event: HTTPRequest = { key: "HTTPRequest", data: { status: 0 } };
+  confirmChannel.publish("event-bus", event.key, Buffer.from(JSON.stringify(event)));
+  await confirmChannel.waitForConfirms();
+  res.send(200);
+});
 
 const updateDatabase = (siteHealthData: SiteHealthData) => {
   return true;
 };
+
+app.listen(4009, () => {
+  console.log("site health service listening on port 4009");
+});
