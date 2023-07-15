cb43724e733b1b83903a08e32607b8d70a849066
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2023-07-15 15:39:29 -0400

    updated url

--- frontend/src/utils/services.ts
+++ frontend/src/utils/services.ts
@@ -1,12 +1,14 @@
-export const RoomsService = "api.letsth.ink:4001";
-export const MessagesService = "api.letsth.ink:4002";
-export const PollsService = "api.letsth.ink:4003";
-export const ModeratorService = "api.letsth.ink:4004";
-export const EmailService = "api.letsth.ink:4005";
-export const UsersService = "api.letsth.ink:4006";
-export const AuthService = "api.letsth.ink:4007";
-export const ExpirationService = "api.letsth.ink:4008";
-export const SiteHealthService = "api.letsth.ink:4009";
-export const VisualizerService = "api.letsth.ink:4010";
-export const QueryService = "api.letsth.ink:4011";
-export const VoteService = "api.letsth.ink:4012";
+const baseUrl = "http://api.letsth.ink";
+
+export const RoomsService = `${baseUrl}:4001`;
+export const MessagesService = `${baseUrl}:4002`;
+export const PollsService = `${baseUrl}:4003`;
+export const ModeratorService = `${baseUrl}:4004`;
+export const EmailService = `${baseUrl}:4005`;
+export const UsersService = `${baseUrl}:4006`;
+export const AuthService = `${baseUrl}:4007`;
+export const ExpirationService = `${baseUrl}:4008`;
+export const SiteHealthService = `${baseUrl}:4009`;
+export const VisualizerService = `${baseUrl}:4010`;
+export const QueryService = `${baseUrl}:4011`;
+export const VoteService = `${baseUrl}:4012`;
